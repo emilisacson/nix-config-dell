3673caffe2ac6e9a38cf0b07a520f927f6a848a9
Emil Isacson <emil.isacson@gmail.com>
2025-06-10 22:27:17 +0200

    Started adding support for vs code insiders

--- applications/vscode.nix
+++ applications/vscode.nix
@@ -1,22 +1,36 @@
 { pkgs, unstable, lib, config, ... }:
 
-{
-  /* Insiders version of VS Code
-     (pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: rec {
-       src = (builtins.fetchTarball {
-         url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-         sha256 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-       });
-       version = "lat est";
+let
+  # Toggle between VS Code Insiders and stable version
+  useInsiders = false; # Set to false to use stable version
 
-       buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 ];
-     });
-  */
+  vscodePackage = if useInsiders then
+  # VS Code Insiders version
+    (pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: rec {
+      src = (builtins.fetchTarball {
+        url =
+          "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
+        sha256 = "sha256:0yad7xdr28lbq4m0h97fapzifjbkb47m6ds6szzalpz8m9lc1hvj";
+      });
+      version = "latest";
+      buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 ];
+      postInstall = (oldAttrs.postInstall or "") + ''
+        # Create a 'code' symlink for VS Code Insiders
+        ln -sf $out/bin/code-insiders $out/bin/code || true
 
-  # VS Code configuration with latest version from Microsoft
-  programs.vscode = {
-    enable = true;
-    package = (pkgs.vscode.override { }).overrideAttrs (oldAttrs: rec {
+        # Create a standard 'code.desktop' entry for GNOME launcher
+        if [ -f $out/share/applications/code-insiders.desktop ]; then
+          cp $out/share/applications/code-insiders.desktop $out/share/applications/code.desktop
+          # Update the desktop entry to use the generic 'code' command
+          sed -i 's/code-insiders/code/g' $out/share/applications/code.desktop
+          sed -i 's/Code - Insiders/Visual Studio Code/g' $out/share/applications/code.desktop
+          sed -i 's/Visual Studio Code - Insiders/Visual Studio Code/g' $out/share/applications/code.desktop
+        fi
+      '';
+    })
+  else
+  # VS Code stable version
+    (pkgs.vscode.override { }).overrideAttrs (oldAttrs: rec {
       src = (builtins.fetchTarball {
         url = "https://update.code.visualstudio.com/latest/linux-x64/stable";
         sha256 = "sha256:1gicmx3lkifigwr6dqf8gghbm1fmiafdzrbw2x5069absji3x6pg";
@@ -24,6 +38,11 @@
       version = "latest";
       buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 pkgs.nixfmt-classic ];
     });
+in {
+  # VS Code configuration with configurable version
+  programs.vscode = {
+    enable = true;
+    package = vscodePackage;
 
     # Allow VS Code to manage extensions
     mutableExtensionsDir = true;
